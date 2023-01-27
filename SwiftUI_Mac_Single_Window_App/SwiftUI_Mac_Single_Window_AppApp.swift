2382727405be0470c371bbc5b4ee6f52f1e0323c
Joseph Wardell <jaywardell@mac.com>
2023-01-27 12:18:53 -0500

    added documentation to explain the need for each line change

--- SwiftUI_Mac_Single_Window_App/SwiftUI_Mac_Single_Window_AppApp.swift
+++ SwiftUI_Mac_Single_Window_App/SwiftUI_Mac_Single_Window_AppApp.swift
@@ -7,12 +7,32 @@
 
 import SwiftUI
 
+
+
 @main
 struct SwiftUI_Mac_Single_Window_AppApp: App {
+    
+    // this line gives us the rest of the features we need
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
+        // using Window instead of WindowGroup gives us
+        // most of the features of a single-window app
         Window("A Single-Window App", id: "Single Window App") {
             ContentView()
         }
         .windowResizability(.contentSize)
+    }
+}
+
+// MARK: -
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+
+    // SwiftUI won't turn off the tabs menu items in the View Menu
+    // but we don't want them for this app
+    // since there's only one window.
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApplication.shared.windows.first?.tabbingMode = .disallowed
     }
 }
